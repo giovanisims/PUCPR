df48f1642532528e14e72ca64ccd8221d320d31c
giovanisims <giovani@simoes.pro>
2024-10-07 11:50:20 -0300

    entrega de python funcoes matdis

--- periodo2/MatDis/01-10-2024/mat_dis2.py
+++ periodo2/MatDis/01-10-2024/mat_dis2.py
@@ -1,12 +1,31 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
+def funcao1grau(a, b, x):
+    return a * x + b
+
+c = 1
+
+vetorX = np.arange(-5, 5, c)
+# print(vetorX)
+
+# 3. a) A função arange cria um vetor com os valores de -5 a 5 (sem incluir o 5) com progressão de 1
 
 a = 2
 b = 5
 
-vetorY = []
+vetorY = np.arange(-5, 5, c)
+# print(vetorY)
 
-def funcao1grau(a,b,x):
-    return a*x + b
+fig = plt.figure(figsize=(10, 10))
+plt.plot(vetorX, vetorY, label="Funcação 1o grau")
+
+plt.show()
+
+# 5. a) É um grafico discreto que representa um função afim
+# 5 .b) É o mesmo grafico só que ele te maior frequencia dos pontos
+# 5 .c) Os pontos ficam frequentes o bastante que a função parece ser continua com o c = 0.05
+# 5 .d) A função vira uma reta continua com a função plot em vez de scatter
+
+
 
