6ef3e6fbcedf62410b4fb2a606e8da502e71d46d
giovanisims <giovani@simoes.pro>
2025-02-26 21:11:52 -0300

    added nav bar

--- periodo3/WebDev-Framework/my-app/src/layout/Menu.tsx
+++ periodo3/WebDev-Framework/my-app/src/layout/Menu.tsx
@@ -3,6 +3,22 @@ import React from "react";
 const menu : React.FC<{}> = () => {
     return (
 <div className="container">
+    <div>
+    <ul className="nav">
+  <li className="nav-item">
+    <a className="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Link</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link" href="#">Link</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
+    </div>
       <div>
       <form>
   <div className="mb-3">
