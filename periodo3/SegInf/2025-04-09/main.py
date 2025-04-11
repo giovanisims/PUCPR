ca9eaed3f645674168e99d32a62ca1961786f61d
giovanisims <giovani@simoes.pro>
2025-04-10 22:21:30 -0300

    this version is more up to par

--- periodo3/SegInf/2025-04-09/main.py
+++ periodo3/SegInf/2025-04-09/main.py
@@ -5,10 +5,10 @@ def filemanager(username):
     while True:
         choice = input('''
 Select a command:
-    1 - Read
-    2 - Write
-    3 - Delete
-    4 - Run
+    1 - Read  
+    2 - Write  
+    3 - Delete  
+    4 - Run  
     5 - Exit 
 ''')
         match choice:
@@ -25,87 +25,65 @@ Select a command:
 
 def checkaccess(username, choice):
     filename = input('Input the filename: ').strip()
-    with open('permission.csv', 'r') as csvfile:
+    with open('permissions.csv', 'r') as csvfile:
         permissionreader = csv.reader(csvfile, delimiter=',')
         next(permissionreader)
         for row in permissionreader:
-            if username == row[0] and filename == row[2]:
-                if row[(choice)+2] == '1':
+            if username == row[0] and filename == row[1]:
+                if row[choice + 1] == '1':
                     print('You can do that!')
-                    return True
+                    return True  
                 else:
                     print('You can\'t do that!')
-                    return False
+                    return False  
         print("No matching permissions found.")
         return False
 
 def login():
-    username = input("input Your username: ").strip()
+    username = input("Input your username: ").strip()
     password = input("Input your password: ").strip()
-    with open('permission.csv', 'r') as csvfile:
+    with open('credentials.csv', 'r') as csvfile:
         loginreader = csv.reader(csvfile, delimiter=',')
         next(loginreader)
         for row in loginreader:
             if username == row[0] and password == row[1]:
-                return username
+                return username  
         return False
 
-
-def get_permission(permission_type):
-    while True:
-        value = input(f'''
-Does the user have {permission_type} permissions:
-    1 - Yes
-    0 - No
-''').strip()
-        if value in ['0', '1']:
-            return value
-        else:
-            print(f'Invalid {permission_type} option')
-
 def signup():
-    with open('permission.csv', 'a', newline='') as csvfile:
+    username = input('Input your username: ').strip()
+    if not username:
+        print('Invalid username')
+        return False
+
+    password = input('Input your password: ').strip()
+    if not password:
+        print('Invalid password')
+        return False
+
+    while True:
+        passwordconfirm = input('Confirm your password: ').strip()
+        if password != passwordconfirm:
+            print('Passwords are different')
+        else:
+            break
+
+    with open('credentials.csv', 'a', newline='') as csvfile:
         signupwriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        signupwriter.writerow([username, password])
 
-        username = input('Input your username: ').strip()
-        if not username:
-            print('Invalid username')
-            return False
-
-        password = input('Input your password: ').strip()
-        if not password:
-            print('Invalid password')
-            return False
-
-        while True:
-            passwordconfirm = input('Confirm your password: ').strip()
-            if password != passwordconfirm:
-                print('Passwords are different')
-            else:
-                break
-
-        file = input('Input the filename you want to access: ').strip()
-        if not file:
-            print('Invalid filename')
-            return False
-
-        read = get_permission('read')
-        write = get_permission('write')
-        delete = get_permission('delete')
-        run = get_permission('run')
-
-        signupinfo = [username, password, file, read, write, delete, run]
-
-        signupwriter.writerow(signupinfo)
+    with open('permissions.csv', 'a', newline='') as csvfile:
+        permissionswriter = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        permissionswriter.writerow([username,"placeholder",0,0,0,0])
 
 def authmenu():
     while True:
         choice = input('''
 --------------------------
 Select an option:
-    1 - Sign-Up
-    2 - Log-In
-    3 - Leave
+    1 - Sign-Up  
+    2 - Log-In  
+    3 - Leave  
 ''')
         match choice:
             case '1':
