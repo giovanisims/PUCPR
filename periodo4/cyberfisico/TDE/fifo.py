a0415a523c6a6b9fdaceea0e002d131e48bbf81c
giovanisims <giovani@simoes.pro>
2025-10-18 16:37:29 -0300

    just added the commented extra lines for the vidoe

--- periodo4/cyberfisico/TDE/fifo.py
+++ periodo4/cyberfisico/TDE/fifo.py
@@ -4,7 +4,7 @@ lista_8quadros = []
 
 lista_paginas1 = [4,3,25,8,19,6,25,8,16,35,45,22,8,3,16,25,7] # qual quadro na memória possuirá a página 7?
 lista_paginas2 = [4,5,7,9,46,45,14,4,64,7,65,2,1,6,8,45,14,11] # qual quadro na memória possuirá a página 11?
-lista_paginas3 = [4,6,7,8,1,6,10,15,16,4,2,1,4,6,12,15,16,11 ] # qual quadro na memória possuirá a página 11?
+lista_paginas3 = [4,6,7,8,1,6,10,15,16,4,2,1,4,6,12,15,16,11] # qual quadro na memória possuirá a página 11?
 
 def mapeamento_memoria(lista_paginas):
 
@@ -23,6 +23,7 @@ def mapeamento_memoria(lista_paginas):
 pass
 def main():
     mapeamento_memoria(lista_paginas1)
-    
+    # mapeamento_memoria(lista_paginas2)
+    # mapeamento_memoria(lista_paginas3)
 
 main()
