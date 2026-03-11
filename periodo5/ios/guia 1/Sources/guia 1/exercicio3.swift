394c26c08a3b8abc40e23bf6154df02a6243169b
giovanisims <giovani@simoes.pro>
2026-03-11 17:14:42 -0300

    exercicio 3

---
+++ periodo5/ios/guia 1/Sources/guia 1/exercicio3.swift
@@ -0,0 +1,8 @@
+
+func exercicio3() {
+    for i in 1...10 {
+        if i % 2 == 0 {
+            print(i)
+        }
+    }
+}
--- periodo5/ios/guia 1/Sources/guia 1/guia_1.swift
+++ periodo5/ios/guia 1/Sources/guia 1/guia_1.swift
@@ -4,6 +4,7 @@
 struct guia_1 {
     static func main() {
         // exercicio1()
-        exercicio2()
+        // exercicio2()
+        exercicio3()
     }
 }
